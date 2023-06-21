92067995d78e0a612033c67a6e1399a1bb0a0847
Rodrigo Michelassi <rodrigomichelassi@MacBook-Pro-de-Rodrigo.local>
2023-06-21 09:08:01 -0300

    fix bugs on the input function

--- graph.cpp
+++ graph.cpp
@@ -36,58 +36,71 @@ void Graph::readGraph(){
     
         node->size = 0;
         node->fragment = fragment;
-        this->adjList.push_back(node);
 
+        this->adjList.push_back(node);
         this->V++;
 
         for(int i = 0; i < this->V - 1; i++){   // iterate through the whole list of vertex 
             // check if the fragment that was read makes an arch with another one we already have
             int digit = 0, k = 0;
             int copyDigit;
+            bool achou = false;
             
-            if(this->adjList[i]->fragment == node->fragment)
-                this->adjList[i]->nodesList.push_back(node);
+            // if(this->adjList[i]->fragment == node->fragment)
+            //     this->adjList[i]->nodesList.push_back(node);
 
             while(digit < this->adjList[i]->fragment.length() && this->adjList[i]->fragment[digit] != node->fragment[k]){
                 digit++;    // iterate through the digits of the words we previously had, to see if any digit is equal to a digit on the new word
+                copyDigit = digit;
+            }
+
+            while(k < node->fragment.length() && digit < this->adjList[i]->fragment.length()){
                 if(this->adjList[i]->fragment[digit] == node->fragment[k]){
-                    copyDigit = digit;
-                    while(this->adjList[i]->fragment[digit] == node->fragment[k]){
-                        digit++;
-                        k++;
-                    }
-                    if(digit >= this->adjList[i]->fragment.length() - 1){
-                        this->adjList[i]->nodesList.push_back(node);
-                        this->E++;
-                        break;
-                    }
-                    else{
-                        k = 0;
-                        digit = copyDigit + 1;
-                    }
+                    digit++;
+                    k++;
+                    achou = true;
+                }
+                else{
+                    k = 0;
+                    digit = copyDigit + 1;
+                    copyDigit++;
+                    achou = false;
                 }
             }
-            digit = k = 0;
 
+            if(digit == this->adjList[i]->fragment.length() && achou){
+                this->adjList[i]->nodesList.push_back(node);
+                this->adjList[i]->size++;
+                this->E++;
+            }
+
+            k = digit = 0;
+            achou = 0;
+            
             // run the same algorithm on the opposite direction CHECK IF CORRECT TOMORROW
-            while(digit < node->fragment.length() && this->adjList[i]->fragment[k] != node->fragment[digit]){
-                digit++;    // iterate through the digits of the words we previously had, to see if any digit is equal to a digit on the new word
-                if(this->adjList[i]->fragment[k] == node->fragment[digit]){
-                    copyDigit = digit;
-                    while(this->adjList[i]->fragment[k] == node->fragment[digit]){
-                        digit++;
-                        k++;
-                    }
-                    if(digit >= this->adjList[i]->fragment.length() - 1){
-                        this->adjList[i]->nodesList.push_back(node);
-                        this->E++;
-                        break;
-                    }
-                    else{
-                        k = 0;
-                        digit = copyDigit + 1;
-                    }
+            while(k < node->fragment.length() && this->adjList[i]->fragment[digit] != node->fragment[k]){
+                k++;    // iterate through the digits of the words we previously had, to see if any digit is equal to a digit on the new word
+                copyDigit = k;
+            }
+            
+            while(k < node->fragment.length() && digit < this->adjList[i]->fragment.length()){
+                if(this->adjList[i]->fragment[digit] == node->fragment[k]){
+                    digit++;
+                    k++;
+                    achou = true;
                 }
+                else{
+                    digit = 0;
+                    k = copyDigit + 1;
+                    copyDigit++;
+                    achou = false;
+                }
+            }
+
+            if(k == node->fragment.length() && achou){
+                node->nodesList.push_back(this->adjList[i]);
+                node->size++;
+                this->E++;
             }
         }
     }
